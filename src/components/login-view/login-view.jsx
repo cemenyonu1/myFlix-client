32b26680aeec15f69eca7eb99ba6a9a6aaf5d387
cemenyonu1 <149470860+cemenyonu1@users.noreply.github.com>
2025-01-29 21:32:17 -0700

    Rearranged code

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,15 +1,64 @@
-export const LoginView = () => {
+import React from "react";
+import { useState } from "react";
+
+const url = "https://charlese-movieapp-71f7e695f2c4.herokuapp.com";
+
+export const LoginView = ({ onLoggedIn }) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            Username: username,
+            Password: password,
+        };
+
+        fetch(url + "/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then((response) => {
+                console.log("Login response: ", response);
+                if (response.user) {
+                    localStorage.setItem("user", JSON.stringify(response.user));
+                    localStorage.setItem("token", JSON.stringify(response.token));
+                    onLoggedIn(response.user, response.token);
+                } else {
+                    alert("User is not registered.");
+                }
+            })
+            .catch((e) => {
+                alert("Something went wrong.");
+            });
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Username:
-                <input type="text" />
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
             </label>
             <label>
                 Password:
-                <input type="text" />
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
             </label>
-            <button type="submit">Submit</button>
+            <button type="submit">Login</button>
         </form>
-    )
-}
+    );
+};
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,29 +2,62 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 const url = "https://charlese-movieapp-71f7e695f2c4.herokuapp.com";
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = JSON.parse(localStorage.getItem("token"));
+    const [user, setUser] = useState(storedUser ? storedUser : null);
+    const [token, setToken] = useState(storedToken ? storedToken : null);
     const [movies, setMovies] = useState([]);
-
     const [selectedMovie, setSelectedMovie] = useState(null);
 
     useEffect(() => {
-        fetch(url + "/movies")
+        if (!token) {
+            return;
+        }
+
+        fetch(url + "/movies", {
+            headers: { Authorization: `Bearer ${token}` },
+        })
             .then((response) => response.json())
-            .then((data) => {
-                const moviesFromApi = data.map((doc) => {
-                    return {
-                        id: doc.key,
-                        title: doc.title,
-                        image: doc.image,
-                        director: doc.director
-                    };
-                });
-                setMovies(moviesFromApi);
-            });
+            .then(
+                (data) => {
+                    const moviesFromApi = data.map((doc) => {
+                        return {
+                            id: doc.key,
+                            title: doc.title,
+                            image: doc.image,
+                            director: doc.director,
+                        };
+                    });
+                    setMovies(moviesFromApi);
+                },
+                [token]
+            );
     }, []);
+
+    if (!user) {
+        return (
+            <>
+                <LoginView
+                    onLoggedIn={(user, token) => {
+                        setUser(user);
+                        setToken(token);
+                    }}
+                />
+                or
+                <SignupView />
+            </>
+        );
+    }
+
+    // if (!user) {
+    //  return <LoginView />;
+    // }
 
     if (selectedMovie) {
         return (
@@ -50,6 +83,15 @@ export const MainView = () => {
                     }}
                 />
             ))}
+            <button
+                onClick={() => {
+                    setUser(null);
+                    setToken(null);
+                    localStorage.clear();
+                }}
+            >
+                Logout
+            </button>
         </React.Fragment>
     );
 };
